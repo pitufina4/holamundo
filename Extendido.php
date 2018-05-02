deba6f888894f908fecf21bbda43973486e87dd7
lidia <lidiarosvega@gmail.com>
2018-05-02 08:46:42 +0200

    first commit

---
+++ Extendido.php
@@ -0,0 +1,10 @@
+
+<div class="row">
+
+    <?php for ($i=1; $i < 12; $i++) { ?>
+
+        <div class="col-md-3">
+            <img  src="Imagenes/foto<?= $i ?>.jpg">
+        </div>
+    <?php } ?>
+</div>
